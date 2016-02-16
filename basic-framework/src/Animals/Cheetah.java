317265bbb9a84dbd40ad49055af2ac52add77f0d
loopypup <loopypup@gmail.com>
2016-02-16 12:41:26 -0500

    Animals.terrains package
    started water
    
    adding in animal/terrain package
    
    made animal variables protected

---
+++ basic-framework/src/Animals/Cheetah.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animals;
+
+import framework.Animal;
+import framework.Mesh;
+import static framework.math3d.math3d.axisRotation;
+import static framework.math3d.math3d.mul;
+import framework.math3d.vec4;
+
+/**
+ *
+ * @author Andrew Polanco
+ */
+public class Cheetah extends Animal{
+
+    public Cheetah(Mesh mesh, vec4 position, float yOffset) {
+        super(mesh, position, yOffset);
+    }
+    
+    protected void takeoff()
+    {
+        mVel = mul(new vec4(1,0,0,0), axisRotation(new vec4(0,1,0,0), mRotY));
+        mVel = mul(mVel, 100);
+        mMoving = true;
+    }
+    
+}
---
+++ basic-framework/src/Animals/Ram.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animals;
+
+import framework.Animal;
+import framework.Mesh;
+import framework.math3d.vec4;
+
+/**
+ *
+ * @author Andrew Polanco
+ */
+public class Ram extends Animal{
+
+    int mDmg = 35;
+    public Ram(Mesh mesh, vec4 position, float yOffset) {
+        super(mesh, position, yOffset);
+    }
+    
+}
